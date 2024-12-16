4c2a14a9df46e9ab425a469a3db04d131940e3f1
user. <shl003@mail.usask.ca>
2024-12-16 15:45:16 -0600

    fix route

--- src/app/payment.service.ts
+++ src/app/payment.service.ts
@@ -36,8 +36,7 @@ export interface Payment {
 })
 export class PaymentService {
 
-  // base_url = 'http://3.137.192.231:8000'
-  base_url = 'http://localhost:8000'
+  base_url = 'http://3.137.192.231:8000'
 
   constructor(
     private readonly http: HttpClient
